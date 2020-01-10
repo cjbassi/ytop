0d550ee5266763b53104d3ed5a08ff85ec2abbbe
Caleb Bassi <calebjbassi@gmail.com>
2020-01-10 13:59:42 -0800

    Refactor cpu and use actual percentages

--- src/widgets/cpu.rs
+++ src/widgets/cpu.rs
@@ -16,19 +16,21 @@ pub struct CpuWidget {
 
 	cpu_count: usize,
 
-	show_average_cpu: bool,
-	show_per_cpu: bool,
+	show_average: bool,
+	show_percpu: bool,
 
-	average_cpu_data: (String, Vec<(f64, f64)>),
-	per_cpu_data: Vec<(String, Vec<(f64, f64)>)>,
+	average_data: Vec<(f64, f64)>,
+	percpu_data: Vec<Vec<(f64, f64)>>,
+
+	average_label: String,
+	percpu_labels: Vec<String>,
+
+	average_collector: cpu::CpuPercentCollector,
+	percpu_collector: cpu::CpuPercentCollector,
 }
 
 impl CpuWidget {
-	pub fn new(
-		update_interval: Ratio<u64>,
-		show_average_cpu: bool,
-		show_per_cpu: bool,
-	) -> CpuWidget {
+	pub fn new(update_interval: Ratio<u64>, show_average: bool, show_percpu: bool) -> CpuWidget {
 		let mut cpu_widget = CpuWidget {
 			title: " CPU Usage ".to_string(),
 			update_interval,
@@ -37,26 +39,31 @@ impl CpuWidget {
 
 			cpu_count: cpu::cpu_count() as usize,
 
-			show_average_cpu,
-			show_per_cpu,
+			show_average,
+			show_percpu,
 
-			average_cpu_data: ("AVRG".to_string(), Vec::new()),
-			per_cpu_data: Vec::new(),
+			average_data: Vec::new(),
+			percpu_data: Vec::new(),
+
+			average_label: "AVRG".to_string(),
+			percpu_labels: Vec::new(),
+
+			average_collector: cpu::CpuPercentCollector::new().unwrap(),
+			percpu_collector: cpu::CpuPercentCollector::new().unwrap(),
 		};
 
-		if !(show_average_cpu || show_per_cpu) {
+		if !(show_average || show_percpu) {
 			if cpu_widget.cpu_count <= 8 {
-				cpu_widget.show_per_cpu = true
+				cpu_widget.show_percpu = true
 			} else {
-				cpu_widget.show_average_cpu = true
+				cpu_widget.show_average = true
 			}
 		}
 
-		if cpu_widget.show_per_cpu {
+		if cpu_widget.show_percpu {
 			for i in 0..cpu_widget.cpu_count {
-				cpu_widget
-					.per_cpu_data
-					.push((format!("CPU{}", i), Vec::new()));
+				cpu_widget.percpu_labels.push(format!("CPU{}", i));
+				cpu_widget.percpu_data.push(Vec::new());
 			}
 		}
 
@@ -67,12 +74,15 @@ impl CpuWidget {
 impl UpdatableWidget for CpuWidget {
 	fn update(&mut self) {
 		self.update_count += 1.0;
-		if self.show_average_cpu {
-			self.average_cpu_data.1.push((self.update_count, 5.0));
+		if self.show_average {
+			let average_percent = self.average_collector.cpu_percent().unwrap();
+			self.average_data
+				.push((self.update_count, average_percent.into()));
 		}
-		if self.show_per_cpu {
+		if self.show_percpu {
+			let percpu_percents = self.percpu_collector.cpu_percent_percpu().unwrap();
 			for i in 0..self.cpu_count {
-				self.per_cpu_data[i].1.push((self.update_count, 5.0));
+				self.percpu_data[i].push((self.update_count, percpu_percents[i].into()));
 			}
 		}
 	}
@@ -85,23 +95,23 @@ impl UpdatableWidget for CpuWidget {
 impl Widget for CpuWidget {
 	fn draw(&mut self, area: Rect, buf: &mut Buffer) {
 		let mut datasets = Vec::new();
-		if self.show_average_cpu {
+		if self.show_average {
 			datasets.push(
 				Dataset::default()
-					.name(&self.average_cpu_data.0)
+					.name(&self.average_label)
 					.marker(Marker::Braille)
 					.style(Style::default().fg(Color::Yellow))
-					.data(&self.average_cpu_data.1),
+					.data(&self.average_data),
 			)
 		}
-		if self.show_per_cpu {
-			for per_cpu_data in self.per_cpu_data.iter() {
+		if self.show_percpu {
+			for i in 0..self.cpu_count {
 				datasets.push(
 					Dataset::default()
-						.name(&per_cpu_data.0)
+						.name(&self.percpu_labels[i])
 						.marker(Marker::Braille)
 						.style(Style::default().fg(Color::Yellow))
-						.data(&per_cpu_data.1),
+						.data(&self.percpu_data[i]),
 				)
 			}
 		}
