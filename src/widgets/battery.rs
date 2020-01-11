03136753a47d2a2bbe03a51fb0a7fe4e52e3b74a
Caleb Bassi <calebjbassi@gmail.com>
2020-01-11 15:06:23 -0800

    Implement battery widget

--- src/widgets/battery.rs
+++ src/widgets/battery.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
+use battery::Manager;
 use num_rational::Ratio;
 use tui::buffer::Buffer;
 use tui::layout::Rect;
-use tui::widgets::Widget;
+use tui::style::{Color, Style};
+use tui::widgets::{Axis, Chart, Dataset, GraphType, Marker, Widget};
 
 use crate::update::UpdatableWidget;
 use crate::widgets::block;
@@ -14,6 +16,8 @@ pub struct BatteryWidget {
 	update_count: u64,
 
 	battery_data: HashMap<String, Vec<(f64, f64)>>,
+
+	manager: Manager,
 }
 
 impl BatteryWidget {
@@ -24,6 +28,8 @@ impl BatteryWidget {
 			update_count: 0,
 
 			battery_data: HashMap::new(),
+
+			manager: Manager::new().unwrap(),
 		}
 	}
 }
@@ -31,6 +37,27 @@ impl BatteryWidget {
 impl UpdatableWidget for BatteryWidget {
 	fn update(&mut self) {
 		self.update_count += 1;
+		let mut current_batteries = Vec::new();
+
+		for battery in self.manager.batteries().unwrap() {
+			let battery = battery.unwrap();
+			let model = battery.model().unwrap();
+			self.battery_data
+				.entry(model.to_string())
+				.or_default()
+				.push((
+					self.update_count as f64,
+					battery.state_of_charge().value as f64,
+				));
+			current_batteries.push(model.to_string());
+		}
+
+		let models: Vec<String> = self.battery_data.keys().cloned().collect();
+		for model in models {
+			if !current_batteries.contains(&model) {
+				self.battery_data.remove(&model);
+			}
+		}
 	}
 
 	fn get_update_interval(&self) -> Ratio<u64> {
@@ -40,6 +67,35 @@ impl UpdatableWidget for BatteryWidget {
 
 impl Widget for BatteryWidget {
 	fn draw(&mut self, area: Rect, buf: &mut Buffer) {
-		block::new().title(&self.title).draw(area, buf);
+		let datasets: Vec<Dataset> = self
+			.battery_data
+			.values()
+			.map(|data| {
+				Dataset::default()
+					.marker(Marker::Braille)
+					.style(Style::default().fg(Color::Yellow))
+					.graph_type(GraphType::Line)
+					.data(&data)
+			})
+			.collect();
+
+		Chart::<String, String>::default()
+			.block(block::new().title(&self.title))
+			.x_axis(Axis::default().bounds([
+				self.update_count as f64 - 100.0,
+				self.update_count as f64 + 1.0,
+			]))
+			.y_axis(Axis::default().bounds([0.0, 100.0]))
+			.datasets(&datasets)
+			.draw(area, buf);
+
+		for (i, data) in self.battery_data.iter().enumerate() {
+			buf.set_string(
+				area.x + 3,
+				area.y + 2 + i as u16,
+				format!("{} {:3.0}%", data.0, data.1.last().unwrap().1),
+				Style::default().fg(Color::Yellow),
+			);
+		}
 	}
 }
